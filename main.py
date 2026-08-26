fb80e5d0e549f75d6860056d15cb3b795bbece47
JesúsGranados151 <jesus.granadosf08@gmail.com>
2026-08-26 10:31:07 -0500

    hoy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-Productos = []
+productos = []
 while True:
   print()
   print("Bienvenido a StockHub")
@@ -8,55 +8,62 @@ while True:
   print("3. Buscar Producto")
   print("4. Eliminar Producto")
   print("5. Salir")
-  opcion = input("Ingrese el número de la acción que desea hacer:")
+  print()
+
+  opcion = input("Ingrese el número de la acción que desea hacer: ")
   print()
 
   if opcion == "1":
-    nombre = input("ingrese el nombre del producto:")
-    cantidad = int(input("Ingrese cantidad:"))
-    Producto = {
+    nombre = input("ingrese el nombre del producto: ")
+    print()
+    cantidad = int(input("Ingrese cantidad: "))
+    producto = {
       "nombre" : nombre,
       "cantidad" : cantidad, }
 
-    Productos.append(Producto)
+    productos.append(producto)
 
     print("Producto agregado.")
 
   elif opcion == "2":
-    print("STOCK:")
-    if len(Productos) == 0:
+    print("---STOCK---")
+    print()
+
+    if len(productos) == 0:
       print("Productos no encontrados.")
     else:
-      for Producto in Productos:
+      for Producto in productos:
         print (f"{Producto['nombre']}: {Producto['cantidad']}")
 
   elif opcion == "3":
-    nombre = input("Ingrese el producto que desea buscar:")
+    nombre = input("Ingrese el producto que desea buscar: ")
+    print()
 
     encontrado = False
 
-    for Producto in Productos:
+    for Producto in productos:
       if Producto ["nombre"].lower() == nombre.lower():
         print(f"Producto encontrado: {Producto['nombre']}")
         print(f"Cantidad disponible: {Producto['cantidad']}")
         encontrado = True
 
-        if not encontrado:
-          print("Producto no encontrado.")
+    if not encontrado:
+      print("Producto no encontrado.")
 
   elif opcion == "4":
-    nombre = input("Ingrese el nombre del producto que desea eliminar:")
+    nombre = input("Ingrese el nombre del producto que desea eliminar: ")
+    print()
 
     encontrado = False
 
-    for Producto in Productos:
-      if Producto ["nombre"].lower() == nombre.lower():
-        Productos.remove(Producto)
+    for Producto in productos:
+      if producto ["nombre"].lower() == nombre.lower():
+        productos.remove(Producto)
         print("Producto Eliminado.")
         encontrado = True
         break
-      if not encontrado:
-        print("Producto no encontrado.")
+    if not encontrado:
+      print("Producto no encontrado.")
   elif opcion == "5":
     print("Gracias por utilizar el programa.")
     break
